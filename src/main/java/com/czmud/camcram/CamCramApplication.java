b071dfc92229351f34be67e8040988a6a82316e0
czmud <caleb.zmuda@protonmail.com>
2022-08-12 21:56:42 -0700

    first commit

---
+++ src/main/java/com/czmud/camcram/CamCramApplication.java
@@ -0,0 +1,13 @@
+package com.czmud.camcram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamCramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamCramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/czmud/camcram/CamCramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czmud.camcram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamCramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
