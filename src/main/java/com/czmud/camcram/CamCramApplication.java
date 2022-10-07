4f8ab0e2d221c0138eaab72ed6c30be22d4ed96b
czmud <caleb.zmuda@protonmail.com>
2022-10-07 11:56:50 -0700

    fix aws-deployment build files

--- src/main/java/com/czmud/camcram/CamCramApplication.java
+++ src/main/java/com/czmud/camcram/CamCramApplication.java
@@ -1,8 +1,11 @@
-
 package com.czmud.camcram;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CamCramApplication {
@@ -10,5 +13,16 @@ public class CamCramApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CamCramApplication.class, args);
 	}
-
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setPort(9090);
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setScheme("http");
+((AbstractAjpProtocol)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+        return tomcat;
+    }
 }
